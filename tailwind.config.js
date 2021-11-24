f51df834f5e1ebfc2a59d2cd3a37f6cecce7c040
celinefay <celinefayad@hotmail.com>
2021-11-24 12:16:47 -0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    container:{
+      center: true,
+      padding: '2rem'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
